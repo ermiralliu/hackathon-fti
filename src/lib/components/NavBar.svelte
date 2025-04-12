da052fb1136e77a85a2e56ac23f97235cdb011c2
Bernard Vulaj <benivulaj@gmail.com>
2025-04-12 11:11:27 +0200

    Navigation Bar demo

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,40 @@
+<script>
+    let tabs = [
+        { name: "Home", link: "/" },
+        { name: "Browse Products", link: "/products" },
+        { name: "Bookmarks", link: "/bookmarks" },
+        { name: "Panel", link: "/panel" }
+    ];
+</script>
+
+<style>
+    nav {
+        background-color: #333;
+        padding: 1rem;
+    }
+    ul {
+        display: flex;
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        margin: 0 1rem;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
+
+<nav>
+    <ul>
+        {#each tabs as tab}
+            <li><a href={tab.link}>{tab.name}</a></li>
+        {/each}
+    </ul>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+
+  import NavBar from "../lib/components/NavBar.svelte";
+
+</script>
+<!-- <NavBar></NavBar> -->
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
