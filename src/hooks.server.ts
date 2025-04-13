7890ba42e4f7e6dbc373938efc225a1607d3f80b
ermiralliu <mirialliu@gmail.com>
2025-04-13 06:41:41 +0200

    Limited access to certain routes based on roles

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,39 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { getUserBySessionId } from '$lib/services/auth.service';
 import { getSessionIdFromCookie } from '$lib/helpers/session.helper';
 
-export const handle: Handle = async ({ event, resolve }) => {
-  const sessionId = getSessionIdFromCookie(event.cookies);
+const allAllowed = ['/products', '/login', '/register']; // top level routes
+// const consumerDefault = '/consumer';
+// const farmerDefault = '/farmer';
+// const adminDefault = '/admin';
 
-  if (sessionId) {
-    const user = await getUserBySessionId(sessionId);
-    if (user && event.locals.user !== user) {
+export const handle: Handle = async ({ event, resolve }) => {
+  let user = null;
+  let sessionId: string | undefined;
+  if (!event.locals.user && (sessionId = getSessionIdFromCookie(event.cookies))) {
+    user = await getUserBySessionId(sessionId);
+    if (user) {
       event.locals.user = user; // Make user data available in load functions and API routes
     }
     // console.log(JSON.stringify(user));
   }
+  // Check if the requested path is a protected top-level route
+  const isTopRouteOfCurrentUrl  = (route: string) => event.url.pathname.startsWith(route) 
+    ||  (event.url.pathname === route);
+  console.log(event.url.pathname);
+  const isPublic = allAllowed.some((e)=> event.url.pathname.startsWith(e)) || event.url.pathname === '/';
 
+  if(isPublic)
+    return await resolve(event);
+
+  if (!user) {
+    if (!allAllowed.includes(event.url.pathname))
+      throw redirect(303, '/login');
+    return await resolve(event);
+  } else if ( user.role === 'farmer' && !isTopRouteOfCurrentUrl('/panel')) {
+    throw redirect(303, '/panel/add');
+  } else if( user.role === 'consumer' && !isTopRouteOfCurrentUrl('/consumer')) {
+    throw redirect(303, '/consumer/purchases/1');
+  } 
   return await resolve(event);
 }
--- src/routes/admin/panel/[farmerId]/view/[pageNr]/+page.server.ts
+++ src/routes/admin/panel/[farmerId]/view/[pageNr]/+page.server.ts
@@ -1,7 +1,7 @@
 import { paginateProductsForFarmer } from '$lib/services/adminOnFarmer.service.js';
 import { redirect } from '@sveltejs/kit';
 
-export async function load({params}){
+export async function load({params, locals}){
   const farmerId = Number(params.farmerId);
   let page = Number(params.pageNr);
   // ne rastin e perdorimit te stringave te pavlefshme ne url, do kemi redirect ne faqen e pare (me baze)
